01e063f0a0a6a9b3c6e63e8e8db540872c954bac
Thiago <thigoluis@gmail.com>
2021-10-11 15:44:45 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       Gato gato = new Gato();
+        System.out.println(gato);
+        /* int a = 5;
+        int b = 10;
+        System.out.println("Hello world!" (+ a + b));*/
+
+    }
+}
